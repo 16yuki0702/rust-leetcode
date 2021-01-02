cc4b92fcc4bfdff0c05fa03cd944da84abbd082c
16yuki0702 <hs19870702@gmail.com>
2021-01-02 15:26:18 +0900

    Add cases

--- src/lib.rs
+++ src/lib.rs
@@ -1413,3 +1413,43 @@ impl Solution {
         );
     }
 }
+
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+// Balanced Binary Tree
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        fn dfs(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+            match root {
+                Some(root) => {
+                    let left = dfs(root.borrow().left.clone());
+                    let right = dfs(root.borrow().right.clone());
+                    if (left - right).abs() > 1 || left == -1 || right == -1 {
+                        return -1;
+                    }
+                    left.max(right) + 1
+                }
+                None => 0,
+            }
+        }
+        dfs(root) != -1
+    }
+}
